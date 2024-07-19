ae3a57880cac8b09d0a194e707658d8e0c3d207f
Igor Silva <igor-gab@hotmail.com>
2024-07-19 18:37:07 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -1,61 +1,31 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"net"
 	"os"
 )
 
 func main() {
-	server := newServer()
-	server.Start()
-}
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
 
-type Server struct {
-	listener net.Listener
-	quitch   chan struct{}
-}
-
-func newServer() *Server {
-	return &Server{
-		quitch: make(chan struct{}),
-	}
-}
-func (s *Server) Start() {
-	fmt.Println("Logs from your program will appear here!")
-	l, err := net.Listen("tcp", "localhost:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	defer l.Close()
-	s.listener = l
-	go s.acceptLoop()
-	<-s.quitch
-}
-func (s *Server) acceptLoop() {
-	for {
-		conn, err := s.listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-		go s.readLoop(conn)
+
+	conn, err := l.Accept()
+
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
 	}
-}
-func (s *Server) readLoop(conn net.Conn) {
-	defer conn.Close()
+
 	for {
 		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
-		if errors.Is(err, io.EOF) {
-			fmt.Println("Client closed the connections:", conn.RemoteAddr())
-			break
-		} else if err != nil {
-			fmt.Println("Error while reading the message")
-		}
+
+		conn.Read(buf)
+
 		conn.Write([]byte("+PONG\r\n"))
 	}
 }
