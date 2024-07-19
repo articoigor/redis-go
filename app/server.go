fcd3415be262717b25ec3299939811cbbbf1ef36
Igor Silva <igor-gab@hotmail.com>
2024-07-19 18:26:14 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -8,21 +8,42 @@ import (
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
-	conn, _ := l.Accept()
+	fmt.Println("Listening on 0.0.0.0:6379")
 
-	bytes := make([]byte, 256)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept connection")
+			continue
+		}
 
-	_, _ = conn.Read(bytes)
+		go handleConnection(conn)
+	}
+}
 
-	req := string(bytes)
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
-	fmt.Println(req)
+	for i := 0; i < 2; i++ {
+		bytes := make([]byte, 256)
+		_, err := conn.Read(bytes)
+		if err != nil {
+			fmt.Println("Failed to read from connection")
+			return
+		}
 
-	conn.Write([]byte("+PONG\r\n"))
+		req := string(bytes)
+		fmt.Println(req)
+
+		if req[:4] == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		} else {
+			conn.Write([]byte("-ERR unknown command\r\n"))
+		}
+	}
 }
