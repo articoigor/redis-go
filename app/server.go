58f7a2d6c90cb3baec174a6ddfbc79bffb498272
Igor Silva <igor-gab@hotmail.com>
2024-07-20 04:38:01 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -46,8 +46,6 @@ func handlePings(conn net.Conn, connID int) {
 	for {
 		pingCount++
 
-		fmt.Printf("Connection %d: %d pings received\n", connID, pingCount)
-
 		_, err := conn.Write([]byte("+PONG\r\n"))
 
 		if err != nil {
