2abbfd0142eab7c6bd6fec36734cba8c8cf9c946
Igor Silva <igor-gab@hotmail.com>
2024-07-22 04:42:20 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -101,6 +101,8 @@ func processGetRequest(data []string, hashMap map[string]string) string {
 func processSetRequest(data []string, hashMap map[string]string) string {
 	key, value := data[4], data[5]
 
+	fmt.Println("AQUI")
+
 	fmt.Printf("Key: %s, Value: %s", key, value)
 	hashMap[key] = value
 
