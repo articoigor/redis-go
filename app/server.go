3b05ff4ddff80ef6cb216a8d1e6873c35d84acd7
Igor Silva <igor-gab@hotmail.com>
2024-07-22 04:48:03 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -76,7 +76,7 @@ func handleCommand(conn net.Conn, connID int) {
 }
 
 func processRequest(data []string, hashMap map[string]string) string {
-	endpoint := data[0]
+	endpoint := data[2]
 	fmt.Println(endpoint)
 	switch endpoint {
 	case "ECHO":
