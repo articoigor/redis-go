a8888e83ce04c0cd534ad9db44b445b32c531aac
Igor Silva <igor-gab@hotmail.com>
2024-07-24 01:26:39 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -50,6 +50,7 @@ func handleConnections(listener net.Listener, serverRole, masterUri string) {
 	connCount := 0
 
 	for {
+		fmt.Println(len(""))
 		if len(masterUri) == 0 {
 			fmt.Println("entrou aqui")
 			conn, err := listener.Accept()
