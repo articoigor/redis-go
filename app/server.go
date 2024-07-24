228dfd6d76f75ef51e2161b1a79157b38b261903
Igor Silva <igor-gab@hotmail.com>
2024-07-24 02:21:50 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -97,7 +97,7 @@ func sendReplconf(conn net.Conn, port int) {
 
 	conn.Write([]byte(confirmationStr))
 
-	conn.Write([]byte("*3\r\n*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n*3\r\n"))
+	conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n*3\r\n"))
 
 	conn.Write([]byte("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n*3\r\n"))
 
