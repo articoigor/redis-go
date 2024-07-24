b397944d6c5c83013c532cb213030070e910d907
Igor Silva <igor-gab@hotmail.com>
2024-07-24 02:10:18 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -101,7 +101,7 @@ func sendHandshake(masterUri string, port int) {
 func sendReplconf(conn net.Conn, port int) {
 	strPort := strconv.Itoa(port)
 
-	confirmationStr := fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$%d\r\n%s\r\n*3", len(strPort), strPort)
+	confirmationStr := fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$%d\r\n%s\r\n*3\r\n", len(strPort), strPort)
 
 	conn.Write([]byte(confirmationStr))
 
@@ -110,7 +110,7 @@ func sendReplconf(conn net.Conn, port int) {
 	_, err := conn.Read(firstResponse)
 
 	if err == nil {
-		conn.Write([]byte("$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
+		conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n*3"))
 	}
 }
 
