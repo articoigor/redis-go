091114e8ff3d860eaacb607356c1ef32af77da0a
Igor Silva <igor-gab@hotmail.com>
2024-07-24 05:25:38 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -284,14 +284,14 @@ func processSetRequest(data []string, req string, hashMap map[string]HashMap, co
 		fmt.Println(*subscriberPort)
 		fmt.Println(server.replicationId)
 
-		propagateToReplica(hashValue, *subscriberPort)
+		propagateToReplica(hashValue, subscriberPort)
 	}
 }
 
 func propagateToReplica(hashValue HashMap, subscriber *string) {
 	fmt.Printf("Starting propagation of SET method on replica (port %s) !", *subscriber)
 
-	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%s", subscriber))
+	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%s", *subscriber))
 
 	if err != nil {
 		fmt.Println("Error dialing to subscriber:", err.Error())
