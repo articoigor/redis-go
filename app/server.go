148b8a9a97aefd14c696d12b5568770b8a6f1605
Igor Silva <igor-gab@hotmail.com>
2024-07-25 04:06:37 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -145,6 +145,8 @@ func handleCommand(conn net.Conn, server *Server) {
 func processRequest(data []string, req string, serverAdrs *Server, conn net.Conn) {
 	endpoint := data[2]
 
+	fmt.Printf("Processing %s command", endpoint)
+
 	switch endpoint {
 	case "ECHO":
 		conn.Write([]byte(fmt.Sprintf(("+%s\r\n"), data[4])))
@@ -265,10 +267,9 @@ func processReplconf(conn net.Conn, req string, serverAdrs *Server) {
 }
 
 func processPsync(conn net.Conn, server *Server) {
-	fmt.Println("AQUI")
 	emptyRDB, _ := hex.DecodeString("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")
 
 	message := fmt.Sprintf(("+FULLRESYNC %s 0\r\n$%d\r\n%s"), server.replicationId, len(emptyRDB), emptyRDB)
-	fmt.Printf("Replica in %s\r\n", server.replica)
+
 	conn.Write([]byte(message))
 }
