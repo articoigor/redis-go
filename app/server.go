a9c063d683bd16d70fcaef875f44a20f7b59a9c7
Igor Silva <igor-gab@hotmail.com>
2024-07-25 00:12:23 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -53,13 +53,13 @@ func handleConnections(listener net.Listener, masterAddress string, port int) {
 	for {
 		server := Server{role: "master", database: map[string]HashMap{}, replicationId: generateRepId(), replica: "", offset: 0}
 
-		conn, err := listener.Accept()
+		// conn, err := listener.Accept()
 
-		if err != nil {
-			fmt.Println("Error accepting connection:", err.Error())
+		// if err != nil {
+		// 	fmt.Println("Error accepting connection:", err.Error())
 
-			continue
-		}
+		// 	continue
+		// }
 
 		if len(masterAddress) > 0 {
 			sendHandshake(masterAddress, port)
@@ -67,7 +67,7 @@ func handleConnections(listener net.Listener, masterAddress string, port int) {
 			server.role = "subscriber"
 		}
 
-		go handleCommand(conn, &server)
+		// go handleCommand(conn, &server)
 	}
 }
 
